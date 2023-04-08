a24956ef76d9e8e2d905c21d41db3c1c907f1961
Andrea Pullen <apullenb@gmail.com>
2023-04-08 13:10:52 -0500

    add logic for game

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, render_template, session
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 app.secret_key = 'secret123'
@@ -11,15 +11,26 @@ app.secret_key = 'secret123'
 def success():
     number = random.randint(1,100)
     session['number'] = number
+    session['chances'] = 5
     return render_template('index.html')
     
-@app.route('/game', methods=['GET'])
+@app.route('/game', methods=['GET','POST'])
 def start_game():
-    return render_template('game.html', number=session['number'])
+    if not request.form:
+        guess = ''
+        return render_template('game.html', number=session['number'], guess=guess, chances=session['chances'])
+    if not request.form['guessed'].isdigit():
+        guess = 'Not a Number'
+        return render_template('game.html', number=session['number'], guess=guess, chances=session['chances'])
+    else:
+        guess = int(request.form['guessed'])
+        session['chances'] -= 1
+        return render_template('game.html', number=session['number'], guess=guess, chances=session['chances'])
     
-
-   
-
+@app.route('/destroy_session')
+def reset_session():
+    session.clear()
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True, host="localhost", port=5000)
